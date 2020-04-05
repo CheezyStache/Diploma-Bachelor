97ebebcd2554aeb8538f596ccff3b4fbb207f7cc
Oleksyi <cutpaks@ukr.net>
2020-04-05 16:35:13 +0300

    Custom Dashboard

--- WebController/src/views/index.js
+++ WebController/src/views/index.js
@@ -16,16 +16,22 @@ import {
   Tables,
   Tabs,
   Tooltips,
-} from './Base';
+} from "./Base";
 
-import { ButtonDropdowns, ButtonGroups, Buttons, BrandButtons } from './Buttons';
-import Charts from './Charts';
-import Dashboard from './Dashboard';
-import { CoreUIIcons, Flags, FontAwesome, SimpleLineIcons } from './Icons';
-import { Alerts, Badges, Modals } from './Notifications';
-import { Login, Page404, Page500, Register } from './Pages';
-import { Colors, Typography } from './Theme';
-import Widgets from './Widgets';
+import {
+  ButtonDropdowns,
+  ButtonGroups,
+  Buttons,
+  BrandButtons,
+} from "./Buttons";
+import Charts from "./Charts";
+import Dashboard from "./Dashboard";
+import CustomDashboard from "./CustomDashboard";
+import { CoreUIIcons, Flags, FontAwesome, SimpleLineIcons } from "./Icons";
+import { Alerts, Badges, Modals } from "./Notifications";
+import { Login, Page404, Page500, Register } from "./Pages";
+import { Colors, Typography } from "./Theme";
+import Widgets from "./Widgets";
 
 export {
   Badges,
@@ -50,6 +56,7 @@ export {
   Tables,
   Charts,
   Dashboard,
+  CustomDashboard,
   Widgets,
   Jumbotrons,
   Switches,
@@ -66,4 +73,3 @@ export {
   Breadcrumbs,
   Paginations,
 };
-
