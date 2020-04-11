8e8e1e357fbfae97ea9a5c62dba22da0bce34caf
Oleksyi <cutpaks@ukr.net>
2020-04-11 17:52:21 +0300

    SQL-Scripts update

---
+++ Diploma-WebControllerAPI/Diploma-WebControllerAPI/SQL-Scripts/dbo.City.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[City]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL
+)
--- Diploma-WebControllerAPI/Diploma-WebControllerAPI/SQL-Scripts/dbo.Region.sql
+++ Diploma-WebControllerAPI/Diploma-WebControllerAPI/SQL-Scripts/dbo.Region.sql
@@ -1,11 +1,12 @@
-﻿CREATE TABLE [dbo].[Region]
-(
-	[ID] INT NOT NULL PRIMARY KEY, 
-    [Map] INT NOT NULL, 
-    [UtilityID] INT NOT NULL, 
+﻿CREATE TABLE [dbo].[Region] (
+    [ID]               INT NOT NULL,
+    [Map]              INT NOT NULL,
+    [UtilityID]        INT NOT NULL,
     [RecycleFactoryID] INT NOT NULL,
-	CONSTRAINT FK_RegionUtility FOREIGN KEY (UtilityID)
-	REFERENCES [dbo].[Utility](ID),
-	CONSTRAINT FK_RegionRecycleFactory FOREIGN KEY (RecycleFactoryID)
-	REFERENCES [dbo].[RecycleFactory](ID)
-)
+	[CityID]		   INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_RegionUtility] FOREIGN KEY ([UtilityID]) REFERENCES [dbo].[Utility] ([ID]),
+    CONSTRAINT [FK_RegionRecycleFactory] FOREIGN KEY ([RecycleFactoryID]) REFERENCES [dbo].[RecycleFactory] ([ID]),
+	CONSTRAINT [FK_RegionCity] FOREIGN KEY ([CityID]) REFERENCES [dbo].[City] ([ID])
+);
+
